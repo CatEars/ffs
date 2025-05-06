f3491e0a2fe89c780b70d24c658a9cdbc4cd7019
Henrik 'CatEars' Adolfsson <catears13@gmail.com>
2025-05-06 21:04:06 +0200

    Updated tests for directory listing

--- test/file-listing.test.ts
+++ test/file-listing.test.ts
@@ -1,8 +1,31 @@
 import { assert } from "@std/assert/assert";
+import { assertEquals } from "@std/assert";
 import { baseUrl } from "./constants.ts";
+import { HTTP_404_NOT_FOUND } from "../src/utils/http-codes.ts";
+
+type FileListing = {
+  name: string;
+  isFile: boolean;
+};
 
 Deno.test("Can fetch directory contents", async () => {
   const result = await fetch(baseUrl + "/api/directory?path=.");
   const directoryListing = await result.json();
-  assert(directoryListing.some((x: any) => x.name === "deno.json" && x.isFile));
+  assert(
+    directoryListing.some((x: FileListing) =>
+      x.name === "deno.json" && x.isFile
+    ),
+  );
+});
+
+Deno.test("Cannot fetch a super-directory of the store directory", async () => {
+  const result = await fetch(baseUrl + "/api/directory?path=..");
+  await result.text();
+  assertEquals(result.status, HTTP_404_NOT_FOUND);
+});
+
+Deno.test("Cannot fetch / directory", async () => {
+  const result = await fetch(baseUrl + "/api/directory?path=%2F");
+  await result.text();
+  assertEquals(result.status, HTTP_404_NOT_FOUND);
 });
