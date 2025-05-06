0f80cdca81c040fc8d668336c12680027bebfe99
Henrik 'CatEars' Adolfsson <catears13@gmail.com>
2025-05-06 20:58:53 +0200

    Dont just use magic numbers and static root

--- src/file-listing/list-directory.ts
+++ src/file-listing/list-directory.ts
@@ -1,20 +1,22 @@
 import { Router } from "jsr:@oak/oak/router";
 import { fileExistsUnder } from "../utils/file-exists-under.ts";
+import { getStoreRoot } from "../config.ts";
+import { HTTP_404_NOT_FOUND } from "../utils/http-codes.ts";
 
 export function registerDirectoryRoutes(router: Router) {
   router.get("/api/directory", (ctx) => {
     const pathToCheck = ctx.request.url.searchParams.get("path");
     if (!pathToCheck) {
-      ctx.response.status = 404;
+      ctx.response.status = HTTP_404_NOT_FOUND;
       return;
     }
 
-    const root = ".";
+    const root = getStoreRoot();
     const fileOrDirectory = fileExistsUnder(pathToCheck, root);
     if (fileOrDirectory) {
       ctx.response.body = Deno.readDirSync(fileOrDirectory).toArray();
     } else {
-      ctx.response.status = 404;
+      ctx.response.status = HTTP_404_NOT_FOUND;
     }
   });
 }
---
+++ src/utils/http-codes.ts
@@ -0,0 +1,1 @@
+export const HTTP_404_NOT_FOUND = 404;
