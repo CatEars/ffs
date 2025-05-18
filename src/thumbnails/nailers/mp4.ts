c6f14f954a33bc82eee574d6c60c1b6936cb5446
Henrik 'CatEars' Adolfsson <catears13@gmail.com>
2025-05-18 19:09:14 +0200

    Revert to using URL for directory traversal instead of x-effect
    Update thumbnail generation

--- src/thumbnails/nailers/mp4.ts
+++ src/thumbnails/nailers/mp4.ts
@@ -5,15 +5,21 @@ import { dirname } from "@std/path/dirname";
 import { getThumbnailPath } from "../../files/cache-folder.ts";
 import { move } from "@std/fs";
 
-async function getMp4Duration(thumbnail: ThumbnailRequest) {
+type Mp4Info = {
+  duration: number;
+  height: number;
+  width: number;
+};
+
+async function getMp4Duration(thumbnail: ThumbnailRequest): Promise<Mp4Info> {
   const command = new Deno.Command("ffprobe", {
     args: [
       "-v",
       "error",
-      "-select_streams",
-      "v:0",
       "-show_entries",
       "format=duration",
+      "-show_entries",
+      "stream=width,height",
       "-of",
       "default=noprint_wrappers=1:nokey=1",
       thumbnail.filePath,
@@ -21,28 +27,40 @@ async function getMp4Duration(thumbnail: ThumbnailRequest) {
   });
   const result = await command.output();
   const text = new TextDecoder().decode(result.stdout);
-  return Number.parseFloat(text);
+  const [width, height, dur] = text.split("\n");
+  return {
+    duration: parseFloat(dur),
+    height: parseInt(height),
+    width: parseInt(width),
+  };
 }
 
 export async function createMp4Thumbnail(thumbnail: ThumbnailRequest) {
-  const duration = (await getMp4Duration(thumbnail)) || 100;
-  const position = duration * 0.25;
+  const mp4Info = await getMp4Duration(thumbnail);
+  const position = mp4Info.duration * 0.45;
   const outputPath = getThumbnailPath(thumbnail.filePath);
   const tempFile = await Deno.makeTempFile({
     prefix: "ffs_mp4gen",
     suffix: ".webp",
   });
   ensureDir(dirname(outputPath));
+  const thumbnailFilter = mp4Info.height * 1.3 > mp4Info.width
+    ? "scale=320:480"
+    : mp4Info.height > mp4Info.width
+    ? "scale=320:320"
+    : "scale=320:240";
   const command = new Deno.Command("ffmpeg", {
     args: [
-      "-i",
-      thumbnail.filePath,
       "-ss",
       `${position}`,
-      "-vframes",
-      "1",
+      "-i",
+      thumbnail.filePath,
       "-vf",
-      "scale=320:-1",
+      thumbnailFilter,
+      "-frames:v",
+      "1",
+      "-vsync",
+      "vfr",
       "-y",
       tempFile,
     ],
