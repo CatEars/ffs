cc22f3217f662fb96c343244bb1a60b3aff77a91
Henrik 'CatEars' Adolfsson <catears13@gmail.com>
2025-06-02 21:56:57 +0200

    Convert upload-file to use file tree

--- src/file-listing/upload-file.ts
+++ src/file-listing/upload-file.ts
@@ -1,16 +1,18 @@
-import * as path from "jsr:@std/path";
+import * as path from "@std/path";
 import { Router } from "@oak/oak/router";
-import { fileExistsUnder } from "../utils/file-exists-under.ts";
 import { getStoreRoot } from "../config.ts";
 import {
   HTTP_400_BAD_REQUEST,
-  HTTP_403_FORBIDDEN,
+  HTTP_404_NOT_FOUND,
 } from "../utils/http-codes.ts";
 import { baseMiddlewares } from "../base-middlewares.ts";
-import { resolve } from "@std/path/resolve";
 import { apiProtect } from "../security/api-protect.ts";
+import { FileTree } from "../files/file-tree.ts";
+import { logger } from "../logging/logger.ts";
 
 export function registerUploadFileRoute(router: Router) {
+  const fileTree = new FileTree(getStoreRoot());
+
   router.post(
     "/api/file/upload",
     baseMiddlewares(),
@@ -19,34 +21,38 @@ export function registerUploadFileRoute(router: Router) {
       const data = await ctx.request.body.formData();
       const directory = data.get("directory")?.toString() || getStoreRoot();
       const file = data.get("file");
+
       if (!(file instanceof File)) {
         ctx.response.status = HTTP_400_BAD_REQUEST;
         return;
       }
       const name = file.name;
-
-      const root = getStoreRoot();
-      const targetDirectory = resolve(root, directory);
-      if (fileExistsUnder(targetDirectory, root)) {
-        const filePath = resolveUploadFilename(targetDirectory, name);
-        await Deno.writeFile(filePath, file.stream());
-        ctx.response.redirect(
-          ctx.request.headers.get("Referer") || "/file-manager/",
-        );
-      } else {
-        ctx.response.status = HTTP_403_FORBIDDEN;
+      const listing = fileTree.listDirectory(directory);
+      if (listing.type === "none") {
+        ctx.response.status = HTTP_404_NOT_FOUND;
+        return;
       }
+
+      const targetFile = fileTree.resolvePath(directory, file.name);
+      if (targetFile.type === "invalid") {
+        ctx.response.status = HTTP_400_BAD_REQUEST;
+        return;
+      }
+
+      const extName = path.extname(name);
+      let fileName = path.basename(targetFile.fullPath);
+      while (listing.files.some((x) => x.name === fileName)) {
+        fileName = fileName.substring(0, fileName.length - extName.length) +
+          " Copy" + extName;
+      }
+
+      const directoryPath = path.dirname(targetFile.fullPath);
+      const finalPath = path.resolve(directoryPath, fileName);
+      logger.info(`Uploading file to ${finalPath}`);
+      await Deno.writeFile(finalPath, file.stream());
+      ctx.response.redirect(
+        ctx.request.headers.get("Referer") || "/file-manager/",
+      );
     },
   );
-}
-
-function resolveUploadFilename(directory: string, name: string): string {
-  const extName = path.extname(name);
-  let fileName = name;
-  const filesInDir = (Deno.readDirSync(directory)).toArray();
-  while (filesInDir.some((x) => x.name === fileName)) {
-    fileName = fileName.substring(0, fileName.length - extName.length) +
-      " Copy" + extName;
-  }
-  return path.resolve(directory, fileName);
 }
