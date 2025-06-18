9f0aedf9de180fbafb1a6ac24f225e2d14b3d50c
Henrik 'CatEars' Adolfsson <catears13@gmail.com>
2025-06-18 22:16:18 +0200

    Add prettier formatting to js code

--- src/website/views/home/sort-radio.js
+++ src/website/views/home/sort-radio.js
@@ -1,25 +1,24 @@
 class SortRadio extends HTMLElement {
     static get observedAttributes() {
-        return ['imgSrc', 'checked', 'name', 'text']
+        return ['imgSrc', 'checked', 'name', 'text'];
     }
 
     connectedCallback() {
-        this.render()
+        this.render();
     }
 
     render() {
-        const imgSrc = this.getAttribute('imgSrc') || ''
-        const checked = this.getAttribute('checked') || ''
-        console.log('checked is', checked)
-        const name = this.getAttribute('name') || ''
-        const text = this.getAttribute('text') || ''
-        let labelHtml = ''
+        const imgSrc = this.getAttribute('imgSrc') || '';
+        const checked = this.getAttribute('checked') || '';
+        const name = this.getAttribute('name') || '';
+        const text = this.getAttribute('text') || '';
+        let labelHtml = '';
         if (imgSrc) {
-            labelHtml = `<img width="26" src="${imgSrc}" />`
+            labelHtml = `<img width="26" src="${imgSrc}" />`;
         } else if (text) {
-            labelHtml = text
+            labelHtml = text;
         }
-        const checkedHtml = checked ? 'checked="1"' : ''
+        const checkedHtml = checked ? 'checked="1"' : '';
         this.innerHTML = `<div class="form-check form-check-inline">
   <input
     class="form-check-input"
@@ -31,12 +30,12 @@ class SortRadio extends HTMLElement {
   <label class="form-check-label" for="${name}">
     ${labelHtml}
   </label>
-</div>`  
+</div>`;
     }
-    
+
     attributeChangedCallback() {
-        this.render()
+        this.render();
     }
 }
 
-customElements.define('sort-radio', SortRadio)
+customElements.define('sort-radio', SortRadio);
