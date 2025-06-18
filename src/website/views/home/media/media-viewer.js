3d3854e0234ec2a202e2c1a17e4daf15a9cb218d
Henrik 'CatEars' Adolfsson <catears13@gmail.com>
2025-06-18 22:51:57 +0200

    Add basic media viewer for pngs

---
+++ src/website/views/home/media/media-viewer.js
@@ -0,0 +1,44 @@
+class MediaViewer extends HTMLElement {
+    static get observedAttributes() {
+        return ['src'];
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    attributeChangedCallback() {
+        this.render();
+    }
+
+    render() {
+        const src = this.getAttribute('src');
+        console.log('source is', src);
+        if (!src) {
+            return;
+        }
+        const mimeType = src.endsWith('mp4')
+            ? 'video/mp4'
+            : src.endsWith('m4v')
+            ? 'video/m4v'
+            : src.endsWith('png')
+            ? 'image/png'
+            : '';
+
+        const srcPath = `/api/file?path=${encodeURIComponent(src)}`;
+        let mediaHtml = '';
+        if (mimeType.startsWith('image')) {
+            mediaHtml = `<img class="media-holder" src="${srcPath}"></img>`;
+        } else if (mimeType.startsWith('video')) {
+            mediaHtml = `<video class="col-12 media-holder" controls>
+  <source src="${srcPath}" type="${mimeType}" />
+</video>`;
+        }
+
+        this.innerHTML = `<div class="col-12">
+        ${mediaHtml}
+        </div>`;
+    }
+}
+
+customElements.define('media-viewer', MediaViewer);
