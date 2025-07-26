970bc8086a446a5fd9532c9fb90fa17e138eb022
Henrik <CatEars13@gmail.com>
2025-07-26 12:30:19 +0000

    Move makefile to deno.jsonc file

---
+++ scripts/dev-main.ts
@@ -0,0 +1,6 @@
+Deno.env.set("FFS_ENV", "dev")
+Deno.env.set("FFS_STORE_ROOT", ".")
+Deno.env.set("FFS_USERS_FILE", "data/users-file.json")
+Deno.env.set("FFS_CUSTOM_COMMANDS_FILE", "data/sample-custom-commands.json")
+
+await import("../src/main.ts")
