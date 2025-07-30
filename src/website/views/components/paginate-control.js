2d65fb4e47fd065393393bba7f4db5417e97511b
Henrik <CatEars13@gmail.com>
2025-07-30 07:11:15 +0000

    Make web component paginate control same as old control

--- src/website/views/components/paginate-control.js
+++ src/website/views/components/paginate-control.js
@@ -1,39 +1,5 @@
 import { BaseWebComponent } from './base.js';
 
-/**
- <nav>
-    <ul class="pagination">
-        <li class="page-item">
-            <a
-                href="#"
-                @click="currentPage = lowerPageNumberByOne(currentPage)"
-                x-bind:class="currentPage === 1 ? 'page-link disabled' : 'page-link'"
-                >&lt;</a
-            >
-        </li>
-        <template x-for="(idx) in Array.from({ length: getNumPages(files) }, (_, i) => i + 1)">
-            <li class="page-item">
-                <a
-                    href="#"
-                    x-text="idx"
-                    @click="setCurrentPage(idx); currentPage = idx"
-                    x-bind:class="currentPage === idx ? 'page-link disabled' : 'page-link'"
-                ></a>
-            </li>
-        </template>
-        <li class="page-item">
-            <a
-                href="#"
-                @click="currentPage = increasePageNumberByOne(files, currentPage)"
-                x-bind:class="currentPage === getNumPages(files) ? 'page-link disabled' : 'page-link'"
-                >&gt;</a
-            >
-        </li>
-    </ul>
-</nav>
-
- */
-
 class PaginateControl extends BaseWebComponent {
     static get observedAtttributes() {
         return ['page', 'maxPages'];
@@ -42,15 +8,27 @@ class PaginateControl extends BaseWebComponent {
     render(html) {
         const currentPage = Number.parseInt(this.getAttribute('page')) || 1;
         const maxPages = Number.parseInt(this.getAttribute('maxPages')) || 1;
-        function pre() {
-            console.log('back');
-        }
-        function to(num) {
-            console.log('goto');
-        }
-        function post() {
-            console.log('forward');
-        }
+        const to = (page) => {
+            if (page < 1 || page > maxPages || page === currentPage) {
+                return;
+            }
+            this.dispatchEvent(
+                new CustomEvent('page-shift', {
+                    bubbles: true,
+                    composed: true,
+                    detail: {
+                        page,
+                    },
+                })
+            );
+        };
+        const pre = () => {
+            to(currentPage - 1);
+        };
+
+        const post = () => {
+            to(currentPage + 1);
+        };
 
         return html`<div class="row">
             <nav>
@@ -63,7 +41,19 @@ class PaginateControl extends BaseWebComponent {
                             >${'<'}</a
                         >
                     </li>
-                    <li>
+                    ${Array.from({ length: maxPages }).map(
+                        (_, idx) => html`
+                            <li class="page-item">
+                                <a
+                                    href="#"
+                                    class="page-link ${currentPage === idx + 1 ? 'disabled' : ''}"
+                                    onclick=${() => to(idx + 1)}
+                                    >${idx + 1}</a
+                                >
+                            </li>
+                        `
+                    )}
+                    <li class="page-item">
                         <a
                             href="#"
                             class="page-link ${currentPage === maxPages ? 'disabled' : ''}"
