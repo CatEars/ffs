d8c1c653b6f6297737048bbf3581e535b12add02
Henrik <CatEars13@gmail.com>
2025-08-01 09:38:41 +0000

    Fully implement both create file link and get shared files routes

--- src/share-file/create-file-link.ts
+++ src/share-file/create-file-link.ts
@@ -2,11 +2,14 @@ import { Router } from '@oak/oak/router';
 import { apiProtect } from '../security/api-protect.ts';
 import { baseMiddlewares } from '../base-middlewares.ts';
 import { generateHmacForFile as generateHmacForFiles } from '../security/share-protect.ts';
+import { encodeBase64Url } from 'jsr:@std/encoding@^1.0.10/base64url';
 
 export function registerCreateFileShareLink(router: Router) {
     router.post('/api/link', baseMiddlewares(), apiProtect, async (ctx) => {
         const form = await ctx.request.body.formData();
         const paths = form.getAll('path').map((x) => x.toString());
-        return await generateHmacForFiles(paths);
+        const hmac = await generateHmacForFiles(paths);
+        const encodedPaths = encodeBase64Url(JSON.stringify(paths));
+        ctx.response.redirect(`/share-file/view?paths=${encodedPaths}&hmac=${hmac}`);
     });
 }
--- src/share-file/get-shared-file.ts
+++ src/share-file/get-shared-file.ts
@@ -1,4 +1,23 @@
 import { Router } from '@oak/oak/router';
+import { baseMiddlewares } from '../base-middlewares.ts';
+import { shareProtect } from '../security/share-protect.ts';
+import { decodeBase64Url } from 'jsr:@std/encoding@^1.0.10/base64url';
+import { HTTP_400_BAD_REQUEST } from '../utils/http-codes.ts';
+import { getStoreRoot } from '../config.ts';
 
 export function registerGetSharedFilesRoutes(router: Router) {
+    router.get('/api/share-file/download', baseMiddlewares(), shareProtect, async (ctx) => {
+        const paths = JSON.parse(decodeBase64Url(ctx.request.url.searchParams.get('paths') || ''));
+        const index = Number.parseInt(ctx.request.url.searchParams.get('index') || '');
+        if (!paths || (typeof index !== 'number') || isNaN(index) || !paths[index]) {
+            ctx.response.status = HTTP_400_BAD_REQUEST;
+            return;
+        }
+
+        const path = paths[index];
+        await ctx.send({
+            root: getStoreRoot(),
+            path,
+        });
+    });
 }
