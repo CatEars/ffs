27f7febb7c157775b746a4b2162895b40a0eb8cf
Henrik <CatEars13@gmail.com>
2025-08-16 14:11:31 +0000

    Add route and view to make directory

--- src/file-listing/index.ts
+++ src/file-listing/index.ts
@@ -4,6 +4,7 @@ import { registerFileRoutes } from './get-file.ts';
 import { registerUploadFileRoute } from './upload-file.ts';
 import { registerMoveFileRoute } from './move-file.ts';
 import { registerRenameFileRoute } from './rename-file.ts';
+import { registerMakeDirectoryRoute } from './make-directory.ts';
 
 export function registerAllFileListing(router: Router) {
     registerDirectoryRoutes(router);
@@ -11,4 +12,5 @@ export function registerAllFileListing(router: Router) {
     registerUploadFileRoute(router);
     registerMoveFileRoute(router);
     registerRenameFileRoute(router);
+    registerMakeDirectoryRoute(router);
 }
---
+++ src/file-listing/make-directory.ts
@@ -0,0 +1,48 @@
+import { Router } from '@oak/oak';
+import { FfsApplicationState } from '../user-config/index.ts';
+import { baseMiddlewares, protectedMiddlewares } from '../base-middlewares.ts';
+import {
+    HTTP_400_BAD_REQUEST,
+    HTTP_403_FORBIDDEN,
+    HTTP_404_NOT_FOUND,
+} from '../utils/http-codes.ts';
+
+export function registerMakeDirectoryRoute(router: Router<FfsApplicationState>) {
+    router.post(
+        '/api/directory/make',
+        baseMiddlewares(),
+        ...protectedMiddlewares(),
+        async (ctx) => {
+            const fileTree = ctx.state.fileTree;
+            const formData = await ctx.request.body.formData();
+            const path = formData.get('path')?.toString() || '.';
+            const directoryName = formData.get('name')?.toString() || '';
+            if (!directoryName || !path) {
+                ctx.response.status = HTTP_400_BAD_REQUEST;
+                ctx.response.body = {
+                    error: 'You have to supply a valid path and directory name to create',
+                };
+                return;
+            }
+
+            const rootPath = fileTree.listDirectory(path);
+            const resolved = fileTree.resolvePath(path, directoryName);
+            if (rootPath.type === 'none' || resolved.type === 'invalid') {
+                ctx.response.status = HTTP_404_NOT_FOUND;
+                ctx.response.body = { error: 'Could not find path to create directory in' };
+                return;
+            }
+
+            if (resolved.exists) {
+                ctx.response.status = HTTP_403_FORBIDDEN;
+                ctx.response.body = { error: 'Cannot create a directory that already exists' };
+                return;
+            }
+
+            await Deno.mkdir(resolved.fullPath);
+            ctx.response.redirect(
+                ctx.request.headers.get('Referer') || '/home/',
+            );
+        },
+    );
+}
