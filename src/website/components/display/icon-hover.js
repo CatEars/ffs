6f7b56a7f3b2db69b2c033604cbccae500677350
Henrik 'CatEars' Adolfsson <catears13@gmail.com>
2025-09-05 19:21:53 +0200

    Update size of thumbnails

--- src/website/components/display/icon-hover.js
+++ src/website/components/display/icon-hover.js
@@ -8,46 +8,62 @@ class IconHover extends BaseWebComponent {
         const url =
             this.getAttribute('url') ||
             (isAttributeTrue(this.getAttribute('disabled')) ? '' : '/static/svg/image.svg');
+        const styles = html`<style>
+            .outer {
+                position: relative;
+                height: 100%;
+            }
+            label {
+                position: absolute;
+                visibility: hidden;
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                left: 2rem;
+                top: -30px;
+                padding-top: 1rem;
+                padding-bottom: 1rem;
+                padding-left: 1rem;
+                padding-right: 1rem;
+                border: 1px solid grey;
+                background-color: var(--background-color);
+            }
+            .icon-holder {
+                background-image: url('${url}');
+                background-size: cover;
+                background-repeat: no-repeat;
+                width: var(--thumbnail-hover-max-width);
+                height: var(--thumbnail-hover-max-width);
+            }
+            .outer:hover > label {
+                visibility: visible;
+            }
+            .inner {
+                height: 100%;
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+            }
+        </style>`;
+
         if (!url) {
-            return html`<slot></slot>`;
+            return html`${styles}
+                <div class="outer">
+                    <div class="inner">
+                        <slot></slot>
+                    </div>
+                </div>`;
         }
 
         return html`
-            <style>
-                div {
-                    position: relative;
-                }
-                label {
-                    position: absolute;
-                    visibility: hidden;
-                    display: flex;
-                    flex-direction: row;
-                    align-items: center;
-                    left: 2rem;
-                    top: -30px;
-                    padding-top: 1rem;
-                    padding-bottom: 1rem;
-                    padding-left: 1rem;
-                    padding-right: 1rem;
-                    border: 1px solid grey;
-                    background-color: var(--background-color);
-                }
-                .icon-holder {
-                    background-image: url('${url}');
-                    background-size: cover;
-                    background-repeat: no-repeat;
-                    width: 128px;
-                    height: 128px;
-                }
-                div:hover > label {
-                    visibility: visible;
-                }
-            </style>
-            <div>
+            ${styles}
+            <div class="outer">
                 <label>
                     <span class="icon-holder"></span>
                 </label>
-                <slot></slot>
+                <div class="inner">
+                    <slot></slot>
+                </div>
             </div>
         `;
     }
