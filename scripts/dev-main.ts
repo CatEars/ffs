5b60fe293947067a090deda7b254943004b1168f
Henrik 'CatEars' Adolfsson <catears13@gmail.com>
2025-11-22 16:16:44 +0100

    Update dev-main to set a filter for automatic thumbnail searcher

--- scripts/dev-main.ts
+++ scripts/dev-main.ts
@@ -3,6 +3,7 @@ Deno.env.set('FFS_STORE_ROOT', '.');
 Deno.env.set('FFS_USERS_FILE', 'data/users-file.json');
 Deno.env.set('FFS_CUSTOM_COMMANDS_FILE', 'data/sample-custom-commands.json');
 Deno.env.set('FFS_INSTANCE_SECRET', 'VerySecretIndeed');
+Deno.env.set('FFS_THUMBNAIL_FINDER_SKIP_REGEX', '(test_no_thumbnails_images|\\.no_thumbnail\\.)');
 
 const { bundle } = await import('./bundle-component-library.ts');
 await bundle();
