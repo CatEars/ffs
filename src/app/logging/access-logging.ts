aa5fb6effbadbbe761c964ea629a143b515036ce
Henrik 'CatEars' Adolfsson <catears13@gmail.com>
2026-03-19 17:28:29 +0100

    Ensure access logging does not include search params to hide sensitive data by default

--- src/app/logging/access-logging.ts
+++ src/app/logging/access-logging.ts
@@ -3,6 +3,9 @@ import { requestLogger } from './loggers.ts';
 
 export const logAccessRequests: Middleware = (ctx, next) => {
     const origin = ctx.request.headers.get('X-Forwarded-For') || ctx.request.ip;
-    requestLogger.info(origin, '->', ctx.request.url.toString());
+    const url = ctx.request.url;
+    // URLs may contain secret information in search params. Focus on path only
+    const nonSecretUrl = `${url.protocol}://${url.hostname}${url.pathname}`;
+    requestLogger.info(origin, '->', nonSecretUrl);
     return next();
 };
