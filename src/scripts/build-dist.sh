625450271c7f79b6ccc71164e528f503a395d42d
Henrik 'CatEars' Adolfsson <catears13@gmail.com>
2026-04-01 10:57:49 +0200

    Update deno.jsonc with correct release scripts

--- src/scripts/build-dist.sh
+++ src/scripts/build-dist.sh
@@ -5,6 +5,6 @@ THIS_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$THIS_SCRIPT_DIR/../.."
 TAG=$(git describe --tags --abbrev=0)
 
-rm -rf $ROOT_DIR/dist/* 
+rm -rf $ROOT_DIR/dist/*.tar.gz
 docker save catears/ffs:tiny | gzip > dist/ffs.$TAG.tiny.tar.gz
 docker save catears/ffs | gzip > dist/ffs.$TAG.tar.gz
