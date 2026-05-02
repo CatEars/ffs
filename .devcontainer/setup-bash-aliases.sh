f1cb47eda6ba923d7fb109b63ebafe13c231d21f
Henrik <CatEars13@gmail.com>
2026-05-02 07:32:31 +0000

    Fix wrong vendored scripts PATH update in devcontainer

--- .devcontainer/setup-bash-aliases.sh
+++ .devcontainer/setup-bash-aliases.sh
@@ -5,6 +5,6 @@ THIS_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cat >> ~/.bashrc << EOL
 
 export PATH="\$PATH:$THIS_SCRIPT_DIR/bin"
-export PATH="\$PATH:$THIS_SCRIPT_DIR/../scripts/vendor/bin"
+export PATH="\$PATH:$THIS_SCRIPT_DIR/../src/scripts/vendor/bin"
 
 EOL
