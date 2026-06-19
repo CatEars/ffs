08fe4d1dd016429cd4d6d02508d005ea2b8006c9
Henrik <CatEars13@gmail.com>
2026-06-19 19:50:21 +0000

    Add unix inotify experiment

--- src/experiments/fanotify/main.go
+++ src/experiments/fanotify/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"os"
-	"os/signal"
 
 	"golang.org/x/sys/unix"
 )
 
-func main() {
+func fanotify() {
 
 	if len(os.Args) < 2 {
 		fmt.Println("Buddy, you gotta give me a directory man")
@@ -43,6 +43,73 @@ func main() {
 			os.Exit(-1)
 		}
 		fmt.Printf("Read %d bytes\n", n)
-		
+
 	}
+}
+
+type INotifyEvent struct {
+	Wd     int32
+	Mask   uint32
+	Cookie uint32
+	Len    uint32
+	Name   string
+}
+
+func inotify() {
+
+	if len(os.Args) < 2 {
+		fmt.Println("Buddy, you gotta give me a directory man")
+		os.Exit(-1)
+	}
+	target := os.Args[1]
+
+	fmt.Printf("Looking at %s\n", target)
+
+	fd, err := unix.InotifyInit1(unix.IN_CLOEXEC)
+	if err != nil {
+		fmt.Printf("Woops, got error %s from InotifyInit!", err.Error())
+		os.Exit(-1)
+	}
+	defer unix.Close(fd)
+
+	mask := uint32(unix.IN_CLOSE_WRITE | unix.IN_DELETE)
+	_, err = unix.InotifyAddWatch(fd, target, mask)
+	if err != nil {
+		fmt.Printf("Error when marking inotify %s\n", err.Error())
+		os.Exit(-1)
+	}
+
+	buff := make([]byte, 4096)
+	for {
+		n, err := unix.Read(fd, buff)
+		if err != nil {
+			fmt.Printf("Error when reading from inotify %s\n", err.Error())
+			os.Exit(-1)
+		}
+		fmt.Printf("Read %d bytes\n", n)
+		evts := []INotifyEvent{}
+		for idx := 0; idx < n; {
+			evt := INotifyEvent{}
+			evt.Wd = int32(binary.NativeEndian.Uint32(buff[idx : idx+4]))
+			idx += 4
+			evt.Mask = binary.NativeEndian.Uint32(buff[idx : idx+4])
+			idx += 4
+			evt.Cookie = binary.NativeEndian.Uint32(buff[idx : idx+4])
+			idx += 4
+			evt.Len = binary.NativeEndian.Uint32(buff[idx : idx+4])
+			idx += 4
+			evt.Name = string(buff[idx : idx+int(evt.Len)])
+			idx += int(evt.Len)
+			evts = append(evts, evt)
+			if (evt.Mask & unix.IN_CLOSE_WRITE) > 0 {
+				fmt.Printf("File %s updated\n", evt.Name)
+			} else if (evt.Mask & unix.IN_DELETE) > 0 {
+				fmt.Printf("File %s deleted\n", evt.Name)
+			}
+		}
+	}
+}
+
+func main() {
+	inotify()
 }
